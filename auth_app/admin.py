d0d908c123c9aeaeb7e0e7ae1942ef91169e93cd
Patricia Linne <patricia.linne@yahoo.de>
2026-06-15 10:46:36 +0200

    feat: auth app auto-create profile when creating user via admin

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from auth_app.models import User
+from profile_app.models import Profile
 
 # Register your models here.
 
@@ -28,6 +29,15 @@ class UserAdmin(UserAdmin):
             'fields': ('type',),
         }),
     )
+
+    def save_model(self, request, obj, form, change):
+        """
+        Called when the user is saved in the admin interface.
+        """
+        super().save_model(request, obj, form, change)
+        if not change:
+            if not hasattr(obj, 'profile'):
+                Profile.objects.create(user=obj)
 
 
 admin.site.register(User, UserAdmin)
